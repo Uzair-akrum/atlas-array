d4e8fa303786c93a7e3d0542d148da239409c9af
Uzair-akrum <limiaraion@gmail.com>
2023-11-25 16:35:14 +0500

    chore : enabled cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UploadModule } from './upload/upload.module';
+import { UploadModule } from './modules/upload/upload.module';
 import { ConfigModule } from '@nestjs/config';
-import { MessageModule } from './message/message.module';
+import { MessageModule } from './modules/message/message.module';
+import { DatabaseModule } from './common/database/database.provider';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
     UploadModule,
     ConfigModule.forRoot({ isGlobal: true, envFilePath: ['.env'] }),
     MessageModule,
+    DatabaseModule,
+    UserModule
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as AWS from 'aws-sdk';
+import * as cors from 'cors';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   AWS.config.update({
     accessKeyId: 'your-access-key-id',
     secretAccessKey: 'your-secret-access-key',
     region: 'your-region',
   });
+  app.use(
+    cors({
+      origin: '*', // Specify the allowed origin
+      methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify the allowed HTTP methods
+      allowedHeaders: ['Content-Type', 'Authorization'], // Specify the allowed headers
+      credentials: true, // Enable credentials (cookies, authorization headers)
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
